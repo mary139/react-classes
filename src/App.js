7bda060a12d4c01f4bfcb3f2ec42966962380761
maryam alishahi <alishahi.m2591@gmail.com>
2022-07-23 09:02:21 +0430

    lessons 3

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { Component } from "react"
-import Clock from "./components/Clock";
-import Counter from "./components/Counter";
-import EventRaising from "./components/EventRaising";
-import Greeting from "./components/Greeting";
+import React, { useEffect } from "react";
 
+function App() {
+  const [date, setDate] = React.useState(1000);
 
-class App extends Component {
-  constructor() {
-    super()
-    console.log('constructor')
-    this.state = {
-      showGreeting: true,
+  useEffect(() => {
+    console.log('[useEffect] setup');
+
+    // A message signals to reload one of the `list` items
+    document.getElementById('header').addEventListener("click", e => {
+      console.log('[useEffect] websocket message!')
+      setDate(date - 1)
+    });
+
+    // Before next effect runs, close this websocket
+    return () => {
+      document.getElementById('header').removeEventListener('click', () => {
+        console.log('[useEffect] removeEventListener');
+      })
+      console.log('[useEffect] cleanup');
     }
-  }
+  }, [date]);
 
-
-
-  render() {
-    const { showGreeting } = this.state
-    console.log('render', showGreeting);
-    return (
-      <>
-        <Clock />
-      </>
-    )
-  }
+  return (
+    <div>
+      <h1 id='header'>{date}</h1>
+    </div>
+  );
 }
 
 export default App
